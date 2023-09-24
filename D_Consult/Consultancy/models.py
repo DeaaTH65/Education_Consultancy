1630d11a7ec77de575865ad1f37c3edc2bd7e6db
Deaath65 <deaath65@gmail.com>
2023-09-24 09:41:53 +0545

    Message model

--- D_Consult/Consultancy/models.py
+++ D_Consult/Consultancy/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+class Message(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=100)
+    subject = models.CharField(max_length=100)
+    message = models.TextField(max_length=255)
+    
+    def __str__(self):
+        return self.subject
+    
